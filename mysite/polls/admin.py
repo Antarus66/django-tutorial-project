abffaf3f4a53dea9cb9ccdf77debcaf9c6fd60b2
Antarus66 <antarus66@gmail.com>
2024-05-26 17:36:41 +0200

    Improved questions admin field

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -11,9 +11,11 @@ class ChoiceInline(admin.StackedInline):
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
     inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
 
 
 admin.site.register(Question, QuestionAdmin)
